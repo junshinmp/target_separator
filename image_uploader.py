0f1e9024fb35a04b395683a1a5e4dc6bfc508a03
Junshin Purganan <junshinmp@gmail.com>
2026-05-22 13:47:23 -0400

    completed video uploader to api

--- image_uploader.py
+++ image_uploader.py
@@ -17,7 +17,25 @@ rf = Roboflow(api_key=API_KEY)
 project = rf.workspace().project(PROJECT_ID)
 
 spliced_root = Path("raw_dataset")
-
 if not spliced_root.exists():
     print(f"Error: '{spliced_root}' does not exist. Run video splicer first.")
     exit()
+
+for video_dir in spliced_root.iterdir():
+    print("\n---------------------------------------\n")
+    if not video_dir.is_dir():
+        continue
+
+    for image in video_dir.iterdir():
+        print(f"    - Processing image: {image}")
+        try:
+            project.single_upload(
+                image_path=str(image),
+                batch_name=video_dir.name,
+                num_retry_uploads=3
+            )
+            
+        except Exception as e:
+            print(f"        Error: Image {image} could not be uploaded to API")
+
+print(f"Finished uploading all images to cloud.")
